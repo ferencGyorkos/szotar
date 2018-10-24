6ad29cf6a0a1f9f168bc5244241402f2a68a66c7
Ferenc Gyorkos <feri.gyorkos@gmail.com>
2018-10-24 14:23:30 +0200

    change to 1 html

--- app.py
+++ app.py
@@ -6,35 +6,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # if request.form["drop"] == "asc by english":
-
     szotar = datamanager.list_words()
+    return render_template('index.html', szotar=szotar)
+
+
+@app.route('/<order>')
+def order_by(order):
+    szotar = None
+    if order == 'hungarian-asc':
+        szotar = datamanager.list_words_hungarian_asc()
+    elif order == 'hungarian-desc':
+        szotar = datamanager.list_words_hungarian_desc()
+    elif order == 'english-asc':
+        szotar = datamanager.list_words_english_asc()
+    elif order == 'english-desc':
+        szotar = datamanager.list_words_english_desc()
     return render_template("index.html", szotar=szotar)
-
-
-@app.route('/hungarian-asc')
-def hungarian_asc():
-    szotar = datamanager.list_words_hungarian_asc()
-    return render_template("hungarian_asc.html", szotar=szotar)
-
-
-@app.route('/hungarian-desc')
-def hungarian_desc():
-    szotar = datamanager.list_words_hungarian_desc()
-    return render_template("hungarian_desc.html", szotar=szotar)
-
-
-@app.route('/english-asc')
-def english_asc():
-    szotar = datamanager.list_words_english_asc()
-    return render_template("english_asc.html", szotar=szotar)
-
-
-@app.route('/english-desc')
-def english_desc():
-    szotar = datamanager.list_words_hungarian_asc()
-    return render_template("english_desc.html", szotar=szotar)
-
 
 
 @app.route('/add-word', methods=["POST", "GET"])
